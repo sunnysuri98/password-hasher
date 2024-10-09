9d336b0ed32b6b20b32b26cfc21294246db3054d
Sunny <suri.sunny0@gmail.com>
2024-10-09 18:35:14 +0530

    optimize some code in error file by removing multiple write macro

--- src/error.rs
+++ src/error.rs
@@ -9,12 +9,11 @@ pub enum ErrorMessage {
 
 impl fmt::Display for ErrorMessage {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ErrorMessage::LengthError => write!(f, "Password cannot be greater than 64 characters"),
-
-            ErrorMessage::EmptyPasswordError => write!(f, "Password cannot be empty"),
-
-            ErrorMessage::HashingError=> write!(f, "Hashing Error")
-        }
+        let message = match self {
+            ErrorMessage::LengthError => "Password cannot be greater than 64 characters",
+            ErrorMessage::EmptyPasswordError => "Password cannot be empty",
+            ErrorMessage::HashingError => "Hashing Error",
+        };
+        write!(f, "{}", message)
     }
 }
